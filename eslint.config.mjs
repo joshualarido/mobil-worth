11f16fb49d4b02ee5ae293c727081ce9b5202060
joshualarido <joshualarido@gmail.com>
2026-06-07 15:35:57 +0700

    Serve static export from Pages root

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,16 @@ import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
 
 const eslintConfig = [
+  {
+    ignores: [
+      "404.html",
+      "index.html",
+      "*.txt",
+      "_next/**",
+      "_not-found/**",
+      "_not-found.html",
+    ],
+  },
   ...nextCoreWebVitals,
   ...nextTypescript,
 ];
